0594c8a8115a13c0fdfeabc1e469505fa8017175
Parzival8116 <danceblue315@gmail.com>
2025-03-08 21:31:47 -0700

    .\lab_01.py

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,10 @@
-print("hello world")
+print("This is Lab One.")
+print("I have done most of this before.")
+print("\tHowever, there are some things that are good to review like escape code.")
+print("""And also some things that are intresting...
+that
+I
+have
+not 
+seen 
+before!""")
