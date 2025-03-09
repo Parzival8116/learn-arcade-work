8eb3887d40678b3d0c417eb526f131cfe8259fca
Parzival8116 <danceblue315@gmail.com>
2025-03-08 20:32:02 -0700

    .\test.py

---
+++ Testing/test.py
@@ -0,0 +1,12 @@
+print("Hello World!")
+#multiple lines
+print("It was a dark and stormy night.")
+print("Suddenly a shot rang out!")
+#printing quotation marks
+print("Hi! This is a double quote mark: \" We use it for printing.")
+#Escape code \ examples \' \" \t(tab) \r(Carriage Return (Abbreviated as CR, move cursor to the left)) \n (move cursor down)
+print("""You can print
+on multiple
+lines using
+triple
+quotes.""")
